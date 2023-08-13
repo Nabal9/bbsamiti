9580405442d2a09fcb3b713cf8f2089190dd652e
Nabal9 <142115755+Nabal9@users.noreply.github.com>
2023-08-13 15:34:59 +0530

    add files
    whole site file

---
+++ phone.js
@@ -0,0 +1,11 @@
+let burger = document.querySelector('.burger')
+let navbar = document.querySelector('.navbar')
+let rightNav = document.querySelector('.rightNav')
+let navList = document.querySelector('.nav-list')
+
+
+burger.addEventListener('click',()=>{
+    rightNav.classList.toggle('v-class')
+    navList.classList.toggle('v-class')
+    navbar.classList.toggle('h-nav')
+})
